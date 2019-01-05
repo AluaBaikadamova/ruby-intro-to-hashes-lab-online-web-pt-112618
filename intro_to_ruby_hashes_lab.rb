827d2be806839584cd518a416a7b1a2299093cb5
Alua Baikadamova <abaikad00@gmail.com>
2019-01-05 13:23:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,13 +20,7 @@ monopoly =  {
   {
     :pieces=>4,
     :rent_in_dollars=>{},
-    :names=>
-    {
-      :reading_railroad=>{},
-      :pennsylvania_railroad=>{},
-      :b_and_o_railroad=>{},
-      :shortline=>{}
-    }
+    :names=>{}
   }
 }
 end
